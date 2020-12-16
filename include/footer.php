3e279ccf930564385a4610c16c65ab0c81933239
belrah <belrahj3@gmail.com>
2020-12-16 13:50:05 +0100

    re-created the nabvar and footer

--- include/footer.php
+++ include/footer.php
@@ -1,80 +1,121 @@
-    <!--------------------footer section starts here-------------------->
-    <footer class="page-footer scrollspy " width="100% " id="footer "style="background-color: #990011FF">
-        <div class="container ">
-            <div class="row ">
-                <div class="col s12 m6 l3 ">
-                    <a href="https://github.com/belrah "></a>
-                    <h5 class="yellow-text darken-4 ">MealVille</h5>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus doloribus nam odit fugit inventore
-                </div>
-                <d class="col s12 m6 l3 ">
-                    <h5 class="yellow-text darken-4 ">Quick Links</h5>
-                    <ul>
-                        <li>
-                            <a href="#home" class="white-text ">Home</a>
-                        </li>
-                        <li>
-                            <a href="#menu " class="white-text ">
-                                Menu
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#reservations " class="white-text ">Reservations</a>
-                        </li>
-                        <li>
-                            <a href="#team " class="white-text ">Team</a>
-                        </li>
-                        <li>
-                            <a href="#contact " class="white-text ">contact Us</a>
-                        </li>
-                    </ul>
-                </d>
+<!--footer-->
+<!-- <footer class="footer" id="footer">
+ <div class="container"><br>
+  <div class="row">
+    <div class="col-sm-12 text-center">
+      <div class="social-icon">
+        <a href="index.php"><i class="fa fa-facebook"></i></a>
+        <a href="index.php"><i class="fa fa-twitter"></i></a>
+        <a href="index.php"><i class="fa fa-pinterest"></i></a>
+        <a href="index.php"><i class="fa fa-rss"></i></a>
+      </div>
+      <div class="copyright">
+        <p class="white"> copyright &copy; <b>Chocolate & Moer</b></p>
+        <p><i class="fa fa-phone"></i> +00 (123) 456 7890 &nbsp;<i class="fa fa-envelope-o"></i> info@domain.com</p>
+      </div>
+    </div>
+  </div>
+ </div>
+</footer> -->
+<!--end of footer-->
+<!-- </body> -->
+<!---end of body-->
+<!-- </html> -->
 
-                <div class="col s12 m6 l3 ">
-                    <h5 class="yellow-text darken-4 ">Connect</h5>
-                    <ul>
-                        <li>
-                            <a href="# " class="white-text ">
-                                Facebook</a>
-                        </li>
-                        <li>
-                            <a href="#" class="white-text ">
-                              Instergram
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="white-text ">Twitter</a>
-                        </li>
-                        <li>
-                            <a href="#" class="white-text ">Linkedin</a>
-                        </li>
-                    </ul>
-                </div>
-                <d class="col s12 m6 l3 ">
-                    <h5 class="yellow-text darken-4 ">Address</h5>
-                    <ul>
-                        <li class=" "><i class="material-icons ">location</i>Location</li>
-                        <li class=" "><i class="material-icons ">email</i>Email</li>
-                        <li class=" "><i class="material-icons ">phone</i>Phone Number</li>
-                    </ul>
 
-                </d>
+<!-------footer section starts here -->
+<footer class=" pt-4 container-fluid text-white" style="background-color: #990011FF">
+    <div class="text-center ">
+        <div class="row ">
+            <div class="col-md-3 mx-auto">
+                <a class="navbar-brand" href="index.php">
+                    <strong><em class="ml-0 logo">MealVille</em></strong>
+                </a>
+                <p class="mt-2 text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus est, urna egestas viverra pellentesque a aenean nisi gravida. Sed vel at sed leo volutpat elit. </p>
+            </div>
+
+            <div class="col-md-2 mx-auto">
+                <h6 class="text-uppercase font-weight-bold text-white">SERVICES</h6>
+                <hr class="mb-2 mt-0 d-inline-block mx-auto" style="width: 85px; height: 2px; background-color: #00203FFF;  ">
+                <ul class="list-unstyled ">
+                    <li class="my-2">
+                        <a href="shop.html" class="text-white ">Indoor Eatery</a>
+                    </li>
+                    <li class="my-2">
+                        <a href="shop.html " class="text-white ">Delivery</a>
+                    </li>
+                    <li class="my-2">
+                        <a href="shop.html" class="text-white ">Event Hosting</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-md-2 mx-auto  text-white">
+                <h6 class="text-uppercase font-weight-bold ">QUICK LINKS</h6>
+                <hr class="mb-2 mt-0 d-inline-block mx-auto " style="width: 85px; height: 2px;background-color: #00203FFF; ">
+
+                <ul class="list-unstyled ">
+                    <li class="my-2"><a href="index.html " class="text-white ">Home</a></li>
+                    <li class="my-2"><a href="#about " class="text-white ">About</a></li>
+                    <li class="my-2"><a href="#menu " class="text-white ">Menu</a></li>
+                    <li class="my-2"><a href="#reservation" class="text-white ">Reservation</a></li>
+                    <li class="my-2"><a href="#contact " class="text-white ">Contacts</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-3 mx-auto ">
+                <h6 class="text-uppercase font-weight-bold text-white">CONTACTS</h6>
+                <hr class="mb-2 mt-0 d-inline-block mx-auto" style="width: 85px; height: 2px; background-color: #00203FFF;">
+                <ul class="list-unstyled ">
+                    <li class="my-2 ">
+                        <a href="#contact">
+                            <i class="fas fa-home mr-2 text-white ">No 21, Adeniji Street, S/L. Lagos</i style="color: yellow; ">
+                        </a>
+                    </li>
+                    <li class="my-2 ">
+                        <a href="#">
+                            <i class="fas fa-envelope mr-2 text-white "></i> <i style="color: yellow; ">mealville-restaurant@gmail.com</i>
+                        </a>
+                    </li>
+                    <li class="my-2 ">
+                        <a href="#">
+                            <i class="fas fa-phone mr-2 text-white "></i> <i style="color: yellow; ">+2348102108628</i>
+                        </a>
+                    </li>
+                    <li class="my-2 ">
+                        <a href="#">
+                            <i class="fas fa-tty mr-2 text-white "></i> <i style="color: yellow; "> +01 543 214 098</i>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
+    </div>
 
-        <div class="footer-copyright red accent-4 ">
-            <div class="container center-align ">
-                Designed By <span class="blue-text darken-2 ">&copy; MealVille Restaurant</span> for
-                <a href="https://pearlsafrica.org " class="yellow-text ">Pearls Africa</a>
+    <div class=" text-center py-1">
+        <div class="row d-flex justify-content-center ">
+            <div id="copyright " class="text-center text-white ">
+                <p class="mb-0 " style=" color: yellow; font-size: larger; ">Created By RM1-PG-4</p>
+                <small class="font-weight-bolder ">
+                    Copyright © MEALVILLE || All Rights Reserved.
+                </small>
             </div>
         </div>
-    </footer>
-    <!--------------------footer section ends here--------------------->
-    <script src="assets/js/jquery.js "></script>
-    <script src="assets/js/materialize.js "></script>
-    <script src="assets/js/materialize.min.js "></script>
+    </div>
+</footer>
 
 
-    </body>
 
-    </html>
+
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js " integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo " crossorigin="anonymous "></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js " integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1 " crossorigin="anonymous "></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js " integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM " crossorigin="anonymous "></script>
+<script src="assets/JS/script.js "></script>
+</body>
+
+</html>
---
+++ reservations.php
@@ -0,0 +1,16 @@
+<?php
+
+
+//header
+include('include/header.php');
+
+//footer
+include('include/footer.php');
+
+
+
+
+
+
+
+?>
