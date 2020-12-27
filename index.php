14abde79e09770d5691b1141e2370558d82476f3
belrah <belrahj3@gmail.com>
2020-12-27 02:17:12 +0100

    updated hero section

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ require "header.php";
     <div class="row">
         <div class="col-md-12 text-center">
             <a class="logo"><img src="img/mealvillelogo.png" alt="logo"></a>
-            <button type="button" onclick="window.location.href='reservation.php'" class="btn btn-outline-light btn-lg font-weight-bolder text-danger"><em>Make a Reservation Now!</em></button>
         </div>
+        <button type="button" onclick="window.location.href='reservation.php'" class="btn btn-outline-light btn-lg font-weight-bolder text-danger"><em>Make a Reservation Now!</em></button>
         <!-- <div class="col-md-12 text-center">
             <button type="button" onclick="window.location.href='reservation.php'" class="btn btn-outline-light btn-lg font-weight-bolder text-danger"><em>Make a Reservation Now!</em></button>
         </div> -->
