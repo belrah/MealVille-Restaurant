bd9e8e69775cfc636694b91f89fbb70d8df0543a
belrah <belrahj3@gmail.com>
2020-12-30 11:04:10 +0100

    modified reservation page

--- reservation.php
+++ reservation.php
@@ -68,45 +68,8 @@ require "header.php";
             </div>
 
 
-            <h1>Dropdown Checkboxes</h1>
 
-<div class="dropdown" data-control="checkbox-dropdown">
-  <label class="dropdown-label">Select</label>
-  
-  <div class="dropdown-list">
-    <a href="#" data-toggle="check-all" class="dropdown-option">
-      Check All  
-    </a>
-    
-    <label class="dropdown-option">
-      <input type="checkbox" name="dropdown-group" value="Selection 1" />
-      Selection One
-    </label>
-    
-    <label class="dropdown-option">
-      <input type="checkbox" name="dropdown-group" value="Selection 2" />
-      Selection Two
-    </label>
-    
-    <label class="dropdown-option">
-      <input type="checkbox" name="dropdown-group" value="Selection 3" />
-      Selection Three
-    </label>
-    
-    <label class="dropdown-option">
-      <input type="checkbox" name="dropdown-group" value="Selection 4" />
-      Selection Four
-    </label>
-    
-    <label class="dropdown-option">
-      <input type="checkbox" name="dropdown-group" value="Selection 5" />
-      Selection Five
-    </label>      
-  </div>
-</div>
-
-
-
+   
             <div class="form-group">
 		<label>Enter Time Zone</label>
         <select class="form-control" name="time">
