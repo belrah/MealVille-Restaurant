e89a48b6918b72df52775f59e437055447dc4732
belrah <belrahj3@gmail.com>
2020-12-30 01:10:50 +0100

    update

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "loginsystem";
+$servername = "3.9.173.117";
+$dBUsername = "mealville";
+$dBPassword = "ville123";
+$dBName = "mealdb";
 
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
